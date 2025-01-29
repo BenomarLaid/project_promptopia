2d8709ff4030846097f9dae8c13fa174d694d267
BenomarLaid <95710717+BenomarLaid@users.noreply.github.com>
2025-01-29 11:50:46 -0500

    typo

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,50 +1,52 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { useRouter } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
+
+import Form from "@components/Form";
 
 const UpdatePrompt = () => {
   const router = useRouter();
-  const searchParams = new URLSearchParams(window.location.search);
+  const searchParams = useSearchParams();
   const promptId = searchParams.get("id");
 
-  const [post, setPost] = useState({ prompt: "", tag: "" });
+  const [post, setPost] = useState({ prompt: "", tag: "", });
   const [submitting, setIsSubmitting] = useState(false);
 
   useEffect(() => {
-    if (!promptId) return;
-
     const getPromptDetails = async () => {
-      try {
-        const response = await fetch(`/api/prompt/${promptId}`);
-        const data = await response.json();
-        setPost({ prompt: data.prompt, tag: data.tag });
-      } catch (error) {
-        console.error("Erreur lors de la récupération du prompt :", error);
-      }
+      const response = await fetch(`/api/prompt/${promptId}`);
+      const data = await response.json();
+
+      setPost({
+        prompt: data.prompt,
+        tag: data.tag,
+      });
     };
 
-    getPromptDetails();
+    if (promptId) getPromptDetails();
   }, [promptId]);
 
   const updatePrompt = async (e) => {
     e.preventDefault();
     setIsSubmitting(true);
 
-    if (!promptId) {
-      alert("Missing PromptId!");
-      return;
-    }
+    if (!promptId) return alert("Missing PromptId!");
 
     try {
       const response = await fetch(`/api/prompt/${promptId}`, {
         method: "PATCH",
-        body: JSON.stringify({ prompt: post.prompt, tag: post.tag }),
+        body: JSON.stringify({
+          prompt: post.prompt,
+          tag: post.tag,
+        }),
       });
 
-      if (response.ok) router.push("/");
+      if (response.ok) {
+        router.push("/");
+      }
     } catch (error) {
-      console.error("Erreur lors de la mise à jour :", error);
+      console.log(error);
     } finally {
       setIsSubmitting(false);
     }
@@ -52,7 +54,7 @@ const UpdatePrompt = () => {
 
   return (
     <Form
-      type="Edit"
+      type='Edit'
       post={post}
       setPost={setPost}
       submitting={submitting}
